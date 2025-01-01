5a543bf26ad1156d128fe88180849b912126b25a
Nethash0002 <158549873+Nethash0002@users.noreply.github.com>
2025-01-01 23:40:13 +0400

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
 process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_231f4f228e7994aac89ce57cd17df8cb0').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
